0730d1b8e85ac064852c28a6d2d82611d60c1e2c
Trinity Shiloh <trinity.shiloh01@gmail.com>
2026-07-19 00:13:51 -0400

    Fixed class names

--- Deliverable2/src/dataModels/Booking.java
+++ Deliverable2/src/dataModels/Booking.java
@@ -1,8 +1,8 @@
 package dataModels;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import booking.state.BookingState;
 import observer.Observer;
 
 public class Booking implements Observer {
@@ -10,12 +10,12 @@ public class Booking implements Observer {
     private int bookingID;
     private int roomID;
     private double deposit;
-    private BookingState bookingStatus;
+    private BookingStatus bookingStatus;
     private LocalDateTime bookingTime;
     private LocalDateTime bookingEndTime;
 
 
-    public Booking(int bookingID,int roomID,double deposit,BookingState bookingStatus,LocalDateTime bookingTime,LocalDateTime bookingEndTime) {
+    public Booking(int bookingID,int roomID,double deposit,BookingStatus bookingStatus,LocalDateTime bookingTime,LocalDateTime bookingEndTime) {
         this.bookingID = bookingID;
         this.roomID = roomID;
         this.deposit = deposit;
@@ -82,10 +82,43 @@ public class Booking implements Observer {
     }
 
 
-
     @Override
     public void update(Room room) {
 
     }
+    
+
+	public void setBookingID(int bookingID) {
+		this.bookingID = bookingID;
+	}
+
+
+	public void setDeposit(double deposit) {
+		this.deposit = deposit;
+	}
+
+	public BookingStatus getBookingStatus() {
+		return bookingStatus;
+	}
+
+	public void setBookingStatus(BookingStatus bookingStatus) {
+		this.bookingStatus = bookingStatus;
+	}
+
+	public void setBookingTime(LocalDateTime bookingTime) {
+		this.bookingTime = bookingTime;
+	}
+
+	public void setBookingEndTime(LocalDateTime bookingEndTime) {
+		this.bookingEndTime = bookingEndTime;
+	}
+	
+	@Override
+	public String toString() {
+		String stringBookingStatus = bookingStatus.name();
+		String stringBookingTime = bookingTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+		String stringBookingEndTime = bookingEndTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+		return "Booking ID: " + bookingID + ", Booking Deposit: "+ deposit + ", Booking Status: " + stringBookingStatus + ", Booking Start Time: " + stringBookingTime + ", Booking End Time: " + stringBookingEndTime;
+	}
 
 }
