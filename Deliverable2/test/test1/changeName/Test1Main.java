19b9c1de2d7f144522c75f98564be583c2fcfd55
ss-41 <shamir.ssesook@gmail.com>
2026-07-29 20:10:33 -0400

    added test class

---
+++ Deliverable2/test/test1/changeName/Test1Main.java
@@ -0,0 +1,16 @@
+package test1.changeName;
+
+import org.junit.BeforeClass;
+
+public class Test1Main {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
